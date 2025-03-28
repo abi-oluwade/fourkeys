bd7afece14d820d3f7c534173cea5e76d3f7af55
abi-oluwade <55188841+abi-oluwade@users.noreply.github.com>
2025-03-28 23:09:53 +0000

    Update main.py

--- bq-workers/cloud-build-parser/main.py
+++ bq-workers/cloud-build-parser/main.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+r# Copyright 2020 Google LLC
 #d
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
